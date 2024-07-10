98acda011f8cdae453bdba49e15070baf95e9f79
BadKarma99 <saggar.ayush@gmail.com>
2024-07-10 11:17:10 +0530

    meta

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <div className='bg-footerBg w-full flex flex-col items-center md:flex-row justify-between p-6'>
       <div className='flex flex-col  gap-2 mb-6 md:mb-0'>
-        <div className='flex '>
+        <div className='flex justify-center '>
           <Image 
             src="/Icon.jpg"
             alt="icon"
@@ -21,18 +21,18 @@ export default function Footer() {
             height={29}
           />
         </div>
-        <div className='flex flex-col mt-3'>
+        <div className='flex flex-col mt-3 justify-center items-center'>
           <p className='text-gray-500'>Copyright © 2020 Nexcent ltd.</p>
           <p className='text-gray-500'>All rights reserved</p>
         </div>
-        <div className='flex gap-4 mt-2'>
+        <div className='flex gap-4 mt-2 items-center justify-center'>
           <p className='rounded-full text-white'><InstagramLogoIcon /></p>
           <p className='rounded-full text-white'><TwitterLogoIcon /></p>
           <p className='rounded-full text-white'><BsYoutube /></p>
           <p className='rounded-full text-white'><BsPlaystation /></p>
         </div>
       </div>
-      <div className='flex flex-col text-gray-500 mb-6 md:mb-0'>
+      <div className='flex flex-col text-gray-500 mb-6 md:mb-0 justify-center items-center'>
         <p className='text-lg font-medium text-white'>Company</p>
         <p>About Us</p>
         <p>Blog</p>
@@ -40,7 +40,7 @@ export default function Footer() {
         <p>Pricing</p>
         <p>Testimonial</p>
       </div>
-      <div className='flex flex-col text-gray-500 mb-6 md:mb-0'>
+      <div className='flex flex-col text-gray-500 mb-6 md:mb-0 justify-center items-center'>
         <p className='text-lg font-medium text-white'>Support</p>
         <p>Help Center</p>
         <p>Terms of service</p>
@@ -48,8 +48,8 @@ export default function Footer() {
         <p>Privacy Policy</p>
         <p>Status</p>
       </div>
-      <div className='text-white'>
-        <p className='text-lg font-medium'>Stay up to date</p>
+      <div className='text-white '>
+        <p className='text-lg font-medium text-center'>Stay up to date</p>
         <div className='relative mt-2'>
           <input
             className='bg-slate-500 rounded-md h-10 w-full md:w-72 p-2 pr-10 focus-within:text-white focus-within:outline-none text-black'
